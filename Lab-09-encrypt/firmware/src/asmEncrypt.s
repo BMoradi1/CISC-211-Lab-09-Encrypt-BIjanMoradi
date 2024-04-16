77fd4e161dcc875574c5cc29063e6fac81794d6f
BMoradi1 <93304660+BMoradi1@users.noreply.github.com>
2024-04-15 19:16:38 -0700

    Revert "Finished"
    This reverts commit 2f9b92e4e8af8b4164d644fd3ea0d95f3532ad69.

--- Lab-09-encrypt/firmware/src/asmEncrypt.s
+++ Lab-09-encrypt/firmware/src/asmEncrypt.s
@@ -10,14 +10,17 @@
 .type nameStr,%gnu_unique_object
     
 /*** STUDENTS: Change the next line to your name!  **/
-nameStr: .asciz "Bijan Moradi"  
+nameStr: .asciz "Inigo Montoya"  
  
 /* initialize a global variable that C can access to print the nameStr */
 .global nameStrPtr
 .type nameStrPtr,%gnu_unique_object
 nameStrPtr: .word nameStr   /* Assign the mem loc of nameStr to nameStrPtr */
 
-
+ // Define the globals so that the C code can access them
+// (in this lab we return the pointer, so strictly speaking,
+// doesn't really need to be defined as global)
+// .global cipherText
 .type cipherText,%gnu_unique_object
 
 .align
@@ -74,49 +77,14 @@ where:
 .type asmEncrypt,%function
 asmEncrypt:   
 
-  
+    // save the caller's registers, as required by the ARM calling convention
     push {r4-r11,LR}
     
-/* YOUR asmEncrypt CODE BELOW THIS LINE! VVVVVVVVVVVVVVVVVVVVV  */
-    /*NOTE, there was an issue with some of the comments above turning everything orange in the IDE, so I removed them*/
-    LDR r8,= cipherText /*load in the pointer to the destinatioin ciphered text*/
-    cipherLoop:
-	LDRB r7,[r0], 1 /*load the first byte from string pointer in r0, post increment by 1*/
-	CMP r7,0 /*check if we have our null terminator*/
-	BEQ done /*null terminator case, we are done*/
-	CMP r7,'Z' 
-	BGT checkLower /*the ascii value is greater than Z, so we need to check if its within lowercase a-z*/
-	CMP r7, 'A' /*check its range, is it between A-Z?*/
-	BLT dontChange /*not in the cipher range, dont modify*/
-	ADD r7,r7,r1 /*add our shift*/
-	CMP r7, 'Z' /*check if we need to wrap around to A*/
-	BLE store /*it is still within range A-Z, so do not wrap around*/
-	SUB r7,r7,26 /*we passed Z, subtract 26 so we wrap around as per ceaser cipher in slides*/
-	
-	checkLower: /*lets check if we have a lowercase letter*/
-	    CMP r7, 'a' /*check its range, is it between a-z?*/
-	    BLT dontChange
-	    ADD r7,r7,r1 /*add our shift*/
-	    CMP r7, 'z' /*check if we need to wrap around to a*/
-	    BLE store
-	    SUB r7,r7,26 /*we passed z, subtract 26 so we wrap around as per ceaser cipher in slides*/
-	
-	store:
-	    STRB r7,[r8],1 /*store our modified byte then post increment*/
-	    B cipherLoop /*loop into next char, continue until null terminator*/
-	
-	dontChange:
-	    STRB r7,[r8],1 /*char is not in cypher range. dont modify*/
-	    B cipherLoop
-    done:
-	MOV r5,0 /*set r5 to the null byte*/
-	STRB r5,[r8] /*store our null byte*/
-	LDR r0,= cipherText /*pass our memory location in cypher text into r0 as defined in function desc*?
+    /* YOUR asmEncrypt CODE BELOW THIS LINE! VVVVVVVVVVVVVVVVVVVVV  */
     
     /* YOUR asmEncrypt CODE ABOVE THIS LINE! ^^^^^^^^^^^^^^^^^^^^^  */
 
- 
-	
+    // restore the caller's registers, as required by the ARM calling convention
     pop {r4-r11,LR}
 
     mov pc, lr	 /* asmEncrypt return to caller */
